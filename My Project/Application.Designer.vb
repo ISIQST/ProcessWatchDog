234d01ef4942a1ce0cae04f90754690f4802a310
astrigelskiy <as@us-isi.com>
2015-07-24 08:23:24 -0700

    renamed to processwatchdog

--- My Project/Application.Designer.vb
+++ My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.QuasiWatchDog.Form1
+            Me.MainForm = Global.ProcessWatchDog.Form1
         End Sub
     End Class
 End Namespace
